191707bdb191dd67f3e8b056dba40fb2990bbd6e
UnstoppableBlob <unstoppableblob@vivaldi.net>
2026-02-22 13:16:32 -0800

    made chisquared detection function

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
 import {franc} from 'franc'
 
+
+const frequencies = [
+    0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094, 0.06966, 0.00153, 
+    0.00772, 0.04025, 0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056,
+    0.02758, 0.00978, 0.02360, 0.00150, 0.01974, 0.00074
+];
+
+
+function chiSquared(text) {
+    const letters = text.toLowerCase().replace(/[^a-z]/g, '')
+    const totalLetters = letters.length;
+
+    if (totalLetters === 0) return Infinity;
+
+    counts = new Array(26).fill(0)
+
+    for (let i = 0; i < 26; i++) {
+        const charCode = letters.charCodeAt(i) - 97
+        counts[charCode]++
+    }
+
+    let chi2 = 0;
+    for(let i = 0; i < 26; i++) {
+        const observed = counts[i]
+        const expected = totalLetters * frequencies[i]
+
+        if (expected > 0) {
+            chi2 += Math.pow(observed - expected, 2) / expected
+        }
+
+    }
+    return chi2
+}
+
+
 const decrypt = (code, type) => {
     if (type === 'caesar') {
         for (let i = 0; i < 26; i++) {
